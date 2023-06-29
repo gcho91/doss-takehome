e0b0850cfafc61ca9c82570b895ed504c3a9c452
gcho91@gmail.com <gcho91@gmail.com>
2023-06-29 13:36:11 -0700

    Mock adding new workspace

--- src/web/components/WorkspaceList.tsx
+++ src/web/components/WorkspaceList.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import '../style/WorkspaceList.css'
 import DosspaceApi from '../api'
 import { Link } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid'
 
 interface HomepageWorkspace {
   id: string
@@ -11,6 +12,7 @@ interface HomepageWorkspace {
 /** Homepage list of all workspaces that have been created */
 export default function WorkspaceList() {
   const [workspaces, setWorkspaces] = useState<HomepageWorkspace[]>([])
+  const [mockWorkspaces, setMockWorkspaces] = useState<HomepageWorkspace[]>([]);
 
   // Fetch all workspaces from the API
   useEffect(() => {
@@ -21,6 +23,19 @@ export default function WorkspaceList() {
 
     fetchWorkspaces()
   }, [])
+
+
+  const mockAddNewWorkspace = () => {
+    let mockWorkspace = {
+      id: uuidv4(),
+      title: "mock workspace"
+    }
+    setMockWorkspaces([...mockWorkspaces, mockWorkspace])
+  }
+
+  useEffect(() => {
+    console.log(mockWorkspaces)
+  }, [mockWorkspaces])
 
   return (
     <div className="WorkspaceList">
@@ -34,12 +49,48 @@ export default function WorkspaceList() {
             </div>
           </div>
         ))}
-        <div>
-          <button onClick={() => {
-            console.log('adding new workspace')
-          }}>Add new workspace</button>
-        </div>
+
+        {mockWorkspaces.length > 0 &&
+          mockWorkspaces.map((x, ind) => (
+            <div key={x.id} className='mock-workspace-container workspace-container'>
+              <p>{x.title} {ind}</p>
+              <p>{x.id}</p>
+            </div>
+          ))
+        }
+
+      </div>
+      <div className='mock-workspaces'>
+
+      </div>
+
+      <div>
+        <button onClick={() => {
+          console.log('adding new workspace')
+          mockAddNewWorkspace();
+        }}>Add new workspace</button>
       </div>
     </div>
   )
+}
+
+export const AddWorkspaceFormFields = () => {
+
+  const [formData, setFormData] = useState({ name: "", id: "" })
+
+  const handleChange = (event: { target: { name: any; value: any } }) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  return <form onSubmit={() => console.log('handle submit')}>
+    <label htmlFor="name">Name:</label>
+    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+
+    {/* <label htmlFor="id">id:</label>
+    <input type="text" id="id" name="email" value={formData.email} onChange={handleChange} /> */}
+
+    <button type="submit">Submit</button>
+  </form>
+
 }
